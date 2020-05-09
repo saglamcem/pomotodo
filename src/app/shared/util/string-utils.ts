9ee008dbc7925aeb8be290030bd7c56435f6dcb0
Cem Saglam <cemdevp@gmail.com>
2020-05-10 00:33:10 +0200

    Add spacing to seconds to mmss formatter

--- src/app/shared/util/string-utils.ts
+++ src/app/shared/util/string-utils.ts
@@ -2,5 +2,5 @@ export function toMinutesAndSecondsFormat(seconds: number) {
   const format = val => `0${Math.floor(val)}`.slice(-2);
   const minutes = (seconds % 3600) / 60;
 
-  return [minutes, seconds % 60].map(format).join(':');
+  return [minutes, seconds % 60].map(format).join(' : ');
 }
