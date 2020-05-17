368de70a47799c57eced2cbd0d7a33beb8b1417b
Cem Saglam <cemdevp@gmail.com>
2020-05-17 20:52:39 +0200

    Add project-wide constants file

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,19 @@
+const Constants = {
+  TODO: {
+    id: 'todoList',
+    columnTitle: 'To do',
+    state: 'TODO'
+  },
+  CURRENT_FOCUS: {
+    id: 'currentFocus',
+    columnTitle: 'Current focus',
+    state: 'CURRENT_FOCUS'
+  },
+  DONE: {
+    id: 'doneList',
+    columnTitle: 'Done',
+    state: 'DONE'
+  }
+};
+
+export default Constants;
